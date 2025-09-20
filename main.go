07a476156ac11b6381e25127af7c7f7eac27282e
Valery Semenenko <gearmobile@gmail.com>
2025-09-20 20:01:28 +0300

    add packages

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"demo/packages/account"
+	"demo/packages/helpers"
+	"fmt"
+)
+
+func main() {
+	userLogin, userPassword, userUrl := helpers.GetUserInput()
+	account, err := account.UserAccountWithTimeStampConstructor(userLogin, userPassword, userUrl)
+
+	if err != nil {
+		fmt.Println("Error creating account:", err)
+		return
+	}
+
+	result := account.OutputUserData()
+	fmt.Println(result)
+}
